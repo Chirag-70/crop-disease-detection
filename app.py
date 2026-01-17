f83fc7bb23f7ed2ae6a63ee36cfe391efa62f1ea
Chirag Parihar <chiragparihar2102@gmail.com>
2026-01-18 03:37:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -68,8 +68,7 @@ Give short farmer-friendly advice.
         contents=prompt,
         config={                     # ✅ CORRECT KEY
             "max_output_tokens": 1500,
-            "temperature": 0.3,
-            "top_p": 0.9
+          
         }
     )
 
